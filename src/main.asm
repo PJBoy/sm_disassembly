8fde676f941e5b4f7b1f3bcb804dd455d8de3337
InsaneFirebat <InsaneFirebat@gmail.com>
2025-07-30 14:47:51 -0400

    Document ASM files in main.asm

--- src/main.asm
+++ src/main.asm
@@ -29,64 +29,64 @@ endif
 ; or break pointers in SMART for example.
 !ANCHOR_LABELS ?= 1
 
-incsrc "macros.asm"
-incsrc "bank_80.asm"
-incsrc "bank_81.asm"
-incsrc "bank_82.asm"
-incsrc "bank_83.asm"
-incsrc "bank_84.asm"
-incsrc "bank_85.asm"
-incsrc "bank_86.asm"
-incsrc "bank_87.asm"
-incsrc "bank_88.asm"
-incsrc "bank_89.asm"
-incsrc "bank_8A.asm"
-incsrc "bank_8B.asm"
-incsrc "bank_8C.asm"
-incsrc "bank_8D.asm"
-incsrc "bank_8E.asm"
-incsrc "bank_8F.asm"
-incsrc "bank_90.asm"
-incsrc "bank_91.asm"
-incsrc "bank_92.asm"
-incsrc "bank_93.asm"
-incsrc "bank_94.asm"
-incsrc "bank_94..99.asm"
-incsrc "bank_9A.asm"
-incsrc "bank_9B.asm"
-incsrc "bank_9C.asm"
-incsrc "bank_9D.asm"
-incsrc "bank_9E.asm"
-incsrc "bank_9F.asm"
-incsrc "bank_A0.asm"
-incsrc "bank_A1.asm"
-incsrc "bank_A2.asm"
-incsrc "bank_A3.asm"
-incsrc "bank_A4.asm"
-incsrc "bank_A5.asm"
-incsrc "bank_A6.asm"
-incsrc "bank_A7.asm"
-incsrc "bank_A8.asm"
-incsrc "bank_A9.asm"
-incsrc "bank_AA.asm"
-incsrc "bank_AB.asm"
-incsrc "bank_AC.asm"
-incsrc "bank_AD.asm"
-incsrc "bank_AE.asm"
-incsrc "bank_AF.asm"
-incsrc "bank_B0.asm"
-incsrc "bank_B1.asm"
-incsrc "bank_B2.asm"
-incsrc "bank_B3.asm"
-incsrc "bank_B4.asm"
-incsrc "bank_B5.asm"
-incsrc "bank_B6.asm"
-incsrc "bank_B7.asm"
-incsrc "bank_B8.asm"
-incsrc "bank_B9..CE.asm"
-incsrc "bank_CF..DE.asm"
-incsrc "bank_DF.asm"
-incsrc "labels.asm"
-incsrc "memory.asm"
+incsrc macros.asm ; All macros are defined here
+incsrc bank_80.asm ; Game engine
+incsrc bank_81.asm ; SRAM, spritemap handling
+incsrc bank_82.asm ; Gameplay
+incsrc bank_83.asm ; Doors, FX headers
+incsrc bank_84.asm ; PLMs (Post-Load Modification blocks)
+incsrc bank_85.asm ; Message boxes
+incsrc bank_86.asm ; Enemy projectiles
+incsrc bank_87.asm ; Animated tiles
+incsrc bank_88.asm ; HDMA
+incsrc bank_89.asm ; Item graphics, palette blends, FX handler
+incsrc bank_8A.asm ; FX tilemaps, scrolling sky tilemaps
+incsrc bank_8B.asm ; Cinematics/Cutscenes, credits
+incsrc bank_8C.asm ; Cinematic/Cutscene data
+incsrc bank_8D.asm ; Enemy projectile spritemaps, PaletteFX, 
+incsrc bank_8E.asm ; Menu tiles/palettes
+incsrc bank_8F.asm ; PLM populations, room/state headers, room/door ASM, library backgrounds
+incsrc bank_90.asm ; Samus drawing, movement, projectile handling, minimap
+incsrc bank_91.asm ; Samus, demos, transition tables, X-ray, pose handling
+incsrc bank_92.asm ; Samus spritemaps, animation
+incsrc bank_93.asm ; Samus projectiles
+incsrc bank_94.asm ; Block collision, grapple
+incsrc bank_94..99.asm ; Cutscene graphics
+incsrc bank_9A.asm ; Projectile graphics, map graphics
+incsrc bank_9B.asm ; Grapple, Samus graphics
+incsrc bank_9C.asm ; Samus graphics
+incsrc bank_9D.asm ; Samus graphics
+incsrc bank_9E.asm ; Samus graphics
+incsrc bank_9F.asm ; Samus graphics
+incsrc bank_A0.asm ; Enemy handlers
+incsrc bank_A1.asm ; Enemy populations
+incsrc bank_A2.asm ; Enemy AI: Boyon, Stoke, Mama Turtle, Baby Turtle, Puyo, Cacatac, Owtch, Ship, Mellow, Mella, Memu, Multiviola, Polyp, Rinka, Rio, Squeept, Geruta, Holtz, Oum, Choot, Gripper, Ripper II, Dragon, Shutters
+incsrc bank_A3.asm ; Enemy AI: Waver, Metaree, Fireflea, Skultera, Elevator, Sciser, Zero, Tripper, Sbug, Mochtroid, Sidehoppers, Zoa, Viola, Bang, Skree, Yard, Reflec, HZoomer, MZoomer, Zoomer, Zeela, Sova, Metroid
+incsrc bank_A4.asm ; Enemy AI: Crocomire
+incsrc bank_A5.asm ; Enemy AI: Draygon, Spore Spawn
+incsrc bank_A6.asm ; Enemy AI: Ridley, Boulder, Kzan, Hibashi, Puromi, MiniKraid, Steam, Ceres door, Zebetite
+incsrc bank_A7.asm ; Enemy AI: Kraid, Phantoon, Etecoon, Dachora
+incsrc bank_A8.asm ; Enemy AI: Evir, Morph Ball Eye, Fune, Namihe, Coven, Yapping Maw, Kago, Magdollite, Beetom, Powamp, Work Robot, Bull, Alcoon, Atomic, Spark, Face Block, Kihunter
+incsrc bank_A9.asm ; Enemy AI: Mother Brain, Baby Metroid, Torizo corpse, Sidehopper corpse, Zoomer corpse, Ripper corpse, Skree corpse
+incsrc bank_AA.asm ; Enemy AI: Bomb/Golden Torizo, Tourian statue, Shaktool, Maridia tube, Chozo statue
+incsrc bank_AB.asm ; Enemy graphics
+incsrc bank_AC.asm ; Enemy graphics
+incsrc bank_AD.asm ; Enemy graphics
+incsrc bank_AE.asm ; Enemy graphics
+incsrc bank_AF.asm ; Enemy graphics
+incsrc bank_B0.asm ; Enemy graphics
+incsrc bank_B1.asm ; Enemy graphics
+incsrc bank_B2.asm ; Enemy AI: Space pirates
+incsrc bank_B3.asm ; Enemy AI: Zeb, Zebbo, Gamet, Geega, Botwoon, escape Etecoon, escape Dachora
+incsrc bank_B4.asm ; Debug handling, enemy sets/vulnerabilities/drops, sprite objects
+incsrc bank_B5.asm ; Area maps
+incsrc bank_B6.asm ; Pause menu graphics
+incsrc bank_B7.asm ; Enemy graphics
+incsrc bank_B8.asm ; Empty (debug demo recorder writes here)
+incsrc bank_B9..CE.asm ; CRE, backgrounds
+incsrc bank_CF..DE.asm ; SPC engine
+incsrc bank_DF.asm ; Unused music track
+incsrc labels.asm ; Labels that don't point directly to ROM/RAM
+incsrc memory.asm ; Memory labels
 
 print "Assembly complete. Total bytes written: ", bytes
