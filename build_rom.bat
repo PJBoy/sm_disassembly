0777d2de74c34cdea8d8df60aeb1d9f3ed352063
PJBoy <gcupcakekid@gmail.com>
2025-10-16 22:02:33 +0100

    Revert script changes and add create_data_PAL.bat

--- build_rom.bat
+++ build_rom.bat
@@ -1,15 +1,10 @@
-@echo off
-copy /Y FF.sfc "SM JU.sfc" > nul
-copy /Y FF.sfc "SM E.sfc" > nul
 
-@echo on
-"tools/asar" --no-title-check --fix-checksum=off --symbols=wla --symbols-path="symbols NTSC.sym" --define PAL=0 %* src/main.asm "SM JU.sfc"
-@echo off
-if %errorlevel% neq 0 (
-    exit /b %errorlevel%
-)
-@echo on
-"tools/asar" --no-title-check --fix-checksum=off --symbols=wla --symbols-path="symbols PAL.sym" --define PAL=1 %* src/main.asm "SM E.sfc"
 @echo off
 
-exit /b %errorlevel%
+echo Creating FF file
+python tools/ff_file.py ../SM.sfc
+
+echo Patching FF file with asar
+"tools/asar" --no-title-check --symbols=wla --symbols-path=symbols.sym %* src/main.asm SM.sfc && echo Success!
+
+PAUSE
--- create_data_PAL.bat
+++ create_data_PAL.bat
@@ -5,4 +5,4 @@ set "sfc_src=Super Metroid.sfc"
 if "%~1" neq "" set "sfc_src=%~1"
 
 echo Extracting assets from original ROM
-python tools/rip_assets.py "%sfc_src%" -o data || PAUSE
+python tools/rip_assets.py --pal "%sfc_src%" -o data || PAUSE
